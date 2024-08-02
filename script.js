4573989f55a01b4ede0336449d8b8bc482307f0a
Umar Shaikh <158566793+Umar-Shaikh92@users.noreply.github.com>
2024-08-03 02:02:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function firstFloor(){
+let a = document.getElementById("elevator");
+a.style.position = 'absolute';
+a.style.left = '46%';
+a.style.bottom = '20%'; 
+a.style.transition = "3s ease";
+}
+
+function secondFloor(){
+    let a = document.getElementById("elevator");
+    a.style.position = 'absolute';
+    a.style.left = '46%';
+    a.style.bottom = '40%'; 
+    a.style.transition = "3s ease";
+    }
+
+    function thirdFloor(){
+        let a = document.getElementById("elevator");
+        a.style.position = 'absolute';
+        a.style.left = '46%';
+        a.style.bottom = '60%'; 
+        a.style.transition = "3s ease";
+        }
+
+        function fourthFloor(){
+            let a = document.getElementById("elevator");
+            a.style.position = 'absolute';
+            a.style.left = '46%';
+            a.style.bottom = '80%'; 
+            a.style.transition = "3s ease";
+            }
+        function groundFloor(){
+            let a = document.getElementById("elevator");
+            a.style.position = 'absolute';
+            a.style.left = '46%';
+            a.style.bottom = '0%'; 
+            a.style.transition = "3s ease";
+            }
